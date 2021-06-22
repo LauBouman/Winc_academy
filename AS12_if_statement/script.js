092006c42e3ce3619d547258a6cd38d9f816bffd
LauBouman <84869988+LauBouman@users.noreply.github.com>
2021-06-22 19:53:30 +0200

    Add files via upload

---
+++ AS12_if_statement/script.js
@@ -0,0 +1,21 @@
+const age = 24;
+const isFemale = true;
+const driverStatus = 'Bob';
+
+if (age > 18) {
+    document.write("You can enter");
+} else if (age < 18) {
+    document.write("You can not enter");
+}
+
+if (isFemale) {
+    document.write("Is female")
+} else {
+    document.write("Is not female")
+}
+
+if (driverStatus) {
+    document
+}
+
+
