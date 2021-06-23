a5dc26858f06a15a0f3cdc4f56d8cccbba55269c
LauBouman <84869988+LauBouman@users.noreply.github.com>
2021-06-23 19:34:24 +0200

    Add files via upload

---
+++ AS15_funtions_basics/script.js
@@ -0,0 +1,24 @@
+console.log("Hallo Winc Academy studenten")
+
+const makePizza = function() {
+    console.log("Roll dough");
+    console.log("Spread tomato sauce");
+    console.log("Grate cheese");
+    console.log("Bake in oven");
+};
+
+const makeSushi = function() {
+    console.log("Boil rice");
+    console.log("Cut avocado and fish");
+    console.log("Roll sushi");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+    
+
+
+
